7a79dde3529247a94d8071604ade66deecf4d59a
douglas.fazzani@health.telstra.com <douglas.fazzani@health.telstra.com>
2018-09-06 21:07:25 +0100

    Initial Commit

---
+++ DateCalculator/DateTool.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace DateCalculator
+{
+    partial class DateTool
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDateInput = new System.Windows.Forms.TextBox();
+            this.butCalculate = new System.Windows.Forms.Button();
+            this.txtDaysToAdd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDateInput
+            // 
+            this.txtDateInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDateInput.Location = new System.Drawing.Point(25, 51);
+            this.txtDateInput.Name = "txtDateInput";
+            this.txtDateInput.Size = new System.Drawing.Size(164, 29);
+            this.txtDateInput.TabIndex = 0;
+            // 
+            // butCalculate
+            // 
+            this.butCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butCalculate.Location = new System.Drawing.Point(25, 127);
+            this.butCalculate.Name = "butCalculate";
+            this.butCalculate.Size = new System.Drawing.Size(164, 40);
+            this.butCalculate.TabIndex = 2;
+            this.butCalculate.Text = "&Calculate";
+            this.butCalculate.UseVisualStyleBackColor = true;
+            this.butCalculate.Click += new System.EventHandler(this.butCalculate_Click);
+            // 
+            // txtDaysToAdd
+            // 
+            this.txtDaysToAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDaysToAdd.Location = new System.Drawing.Point(261, 51);
+            this.txtDaysToAdd.Name = "txtDaysToAdd";
+            this.txtDaysToAdd.Size = new System.Drawing.Size(100, 29);
+            this.txtDaysToAdd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter a valid date dd/mm/yyyy";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Days to add";
+            // 
+            // txtResults
+            // 
+            this.txtResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResults.Location = new System.Drawing.Point(261, 138);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(164, 29);
+            this.txtResults.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 208);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDaysToAdd);
+            this.Controls.Add(this.butCalculate);
+            this.Controls.Add(this.txtDateInput);
+            this.Name = "Form1";
+            this.Text = "Date Adding Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDateInput;
+        private System.Windows.Forms.Button butCalculate;
+        private System.Windows.Forms.TextBox txtDaysToAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DateCalculator/DateTool.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace DateCalculator
+{
+    public partial class DateTool : Form
+    {
+        public DateTool()
+        {
+            InitializeComponent();
+        }
+
+        private void butCalculate_Click(object sender, EventArgs e)
+        {
+            ValidateDays();
+        }
+
+        private void ValidateDays()
+        {
+            int daysInt;
+            if (Int32.TryParse(txtDaysToAdd.Text, out daysInt))
+                ValidateDate();
+            else
+                MessageBox.Show("Please enter a valid number of days");
+        }
+
+        private void ValidateDate()
+        {
+            if (!string.IsNullOrEmpty(txtDateInput.Text))
+            {
+                DateTime entryDate;
+                if (DateTime.TryParseExact(txtDateInput.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out entryDate))
+                {
+                    ConvertToUnix();
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid date");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid date");
+            }
+        }
+
+        public void ConvertToUnix()
+        {
+            DateTime cDate = Convert.ToDateTime(txtDateInput.Text);
+            Int32 unixTimestamp = (Int32)(cDate.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+            int secs = (24 * 60 * 60) * Convert.ToInt32(txtDaysToAdd.Text);
+
+            unixTimestamp = unixTimestamp + secs;
+            ConvertToCalendar(unixTimestamp);
+        }
+
+        public void ConvertToCalendar(double unixTimeStamp)
+        {
+            System.DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+            
+            dateTime = dateTime.AddSeconds(unixTimeStamp);
+
+            string printDate = dateTime.ToShortDateString();
+
+            txtResults.Text = printDate;
+        }
+
+      }
+
+}
